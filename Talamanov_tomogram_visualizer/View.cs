a8ab5a973c57591539171cf1c0f985b58b828be1
TolyaTalamanov <talamtolik@gmail.com>
2017-04-23 12:11:57 +0300

    try create quadstrip

--- Talamanov_tomogram_visualizer/View.cs
+++ Talamanov_tomogram_visualizer/View.cs
@@ -90,28 +90,54 @@ namespace Talamanov_tomogram_visualizer
         public void DrawQuads(int layerNumber)
         {
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.Begin(BeginMode.Quads);
+            GL.Begin(BeginMode.QuadStrip);
+            short value;
+            for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
+            {
+                //#1
+                value = Bin.array[x_coord  * Bin.X + layerNumber * Bin.X * Bin.Y];
+                GL.Color3(TransferFunction(value));
+                GL.Vertex2(x_coord, 0);
+                //#2
+                value = Bin.array[(x_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                GL.Color3(TransferFunction(value));
+                GL.Vertex2(x_coord,  1);
+                //#3
+                value = Bin.array[(x_coord + 1 + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
+                GL.Color3(TransferFunction(value));
+                GL.Vertex2(x_coord + 1,1);
+                //#4
+                value = Bin.array[(x_coord + 1)  * Bin.X + layerNumber * Bin.X * Bin.Y];
+                GL.Color3(TransferFunction(value));
+                GL.Vertex2(x_coord + 1,0);
+
+                for (int y_coord = 2; y_coord < Bin.Y - 1; y_coord++)
+                {
+                    
+                    //#1
+                    value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord, y_coord);
+                
+                  
+                    //#2
+                    value = Bin.array[x_coord + 1 + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
+                    GL.Color3(TransferFunction(value));
+                    GL.Vertex2(x_coord + 1, y_coord);
+                }
+            }
+            GL.End();
+        }
+        public void DrawQuadStrip(int layerNumber)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GL.Begin(BeginMode.QuadStrip);
             for (int x_coord = 0; x_coord < Bin.X - 1; x_coord++)
             {
                 for (int y_coord = 0; y_coord < Bin.Y - 1; y_coord++)
                 {
                     short value;
-                    //#1
-                    value = Bin.array[x_coord + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord, y_coord);
-                    //#2
-                    value = Bin.array[x_coord + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord, y_coord + 1);
-                    //#3
-                    value = Bin.array[x_coord + 1 + (y_coord + 1) * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord + 1, y_coord + 1);
-                    //#4
-                    value = Bin.array[x_coord + 1 + y_coord * Bin.X + layerNumber * Bin.X * Bin.Y];
-                    GL.Color3(TransferFunction(value));
-                    GL.Vertex2(x_coord + 1, y_coord);
+
                 }
             }
             GL.End();
